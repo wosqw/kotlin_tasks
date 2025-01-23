0748c6273ee58aea4f59ca2f4997238cffed7529
wosqh <144683836+wosqw@users.noreply.github.com>
2025-01-23 10:34:40 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+}
+
+
+// Задание 1
+fun findMax(a: Int, b: Int): Int {
+    if (a == b) {
+        throw IllegalArgumentException("Числа равны.")
+    }
+    return if (a > b) a else b
+}
+
+// Задание 2
+
+// Задание 3
+
+// Задание 4
+
+// Задание 5
+
+// Задание 6
+
+// Задание 7
+
+// Задание
